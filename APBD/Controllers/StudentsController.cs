a3e82ff8b00fd3bd1ef28f963d3a2e2789f59865
s18358 <s18358@pjwstk.edu.pl>
2020-04-08 19:10:21 +0200

    cw3 - to point 4

---
+++ APBD/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalskim, Malewski";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowlaski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono");
+
+        }
+    }
+}
