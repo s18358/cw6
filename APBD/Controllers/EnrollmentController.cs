3b9731276070c2aacb40df5f31f14a90ef3d2d07
s18358 <s18358@pjwstk.edu.pl>
2020-04-24 00:35:33 +0200

    cw 5 - EnrollmentController before Services move

--- APBD/Controllers/EnrollmentController.cs
+++ APBD/Controllers/EnrollmentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using APBD.DTOs.Requests;
@@ -13,11 +14,40 @@ namespace APBD.Controllers
     [ApiController]
     public class EnrollmentController : ControllerBase
     {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s18358;Integrated Security=True";
         [HttpPost]
         public IActionResult EnrollStudent(EnrollStudentRequest request)
         {
 
-            return Ok();
+            using (var con = new SqlConnection(ConString))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                con.Open();
+                var tran = con.BeginTransaction();
+
+                try
+                {
+                    com.CommandText = "select IdStudies from Studies where name=@name";
+                    com.Parameters.AddWithValue("name", request.Studies);
+                    var dr = com.ExecuteReader();
+                    if (!dr.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("Podane studia nie istnieja");
+                    }
+                    int idstudies = (int)dr["IdStudies"];
+
+
+                }
+                catch (SqlException e)
+                {
+                    tran.Rollback();
+                    return BadRequest(e);
+                }
+                tran.Commit();
+                return Ok();
+            }
         }
     }
 }
